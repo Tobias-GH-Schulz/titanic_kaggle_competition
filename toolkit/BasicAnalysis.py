c705482f9eedea8d635a4f0e82493a49b5776e3c
system1c <gyasi@gyasi.com>
2021-01-30 19:53:32 +0100

    updating some files

--- toolkit/BasicAnalysis.py
+++ toolkit/BasicAnalysis.py
@@ -4,8 +4,12 @@ def collectNames(dataframe):
        
 
 def collectgraph(dataframe, columns):
-
+    
     #future feature - ask for a Y variable to drop
+
+    #necessary Packages 
+    import matplotlib.pyplot as plt 
+    import seaborn as sns
     
     #Scaling is needed correctly show all variables distribution at the same time -> Scaling prep
     from sklearn.preprocessing import StandardScaler, MinMaxScaler
@@ -19,8 +23,14 @@ def collectgraph(dataframe, columns):
     new_length = len(list(tempdf.columns))
     dropped_columns = o_length - new_length
 
+    #start one map before scaling
+
+    fig, (ax1) = plt.subplots(ncols=2 , figsize=(20, 8))
+   
+
+
     #scale data
-    tempdf = scaler.fit_transform(tempdf)
+    tempdf = pd.DataFrame(scaler.fit_transform(tempdf),index=tempdf.index, columns=tempdf.columns)
 
     #warn of dropped columns
     if (dropped_columns > 0):
@@ -29,16 +39,20 @@ def collectgraph(dataframe, columns):
 
     #Print preliminary info of original data frame
     print(dataframe.info())
-    print(dataframe.isna().sum())
-    print(dataframe.isna().sum() * 100)
-
-    
+   
     #graphing
-    fig, (ax1) = plt.subplots(ncols=1, figsize=(10, 8))
-    ax1.set_title('Original Distributions')
-    sns.kdeplot(data = tempdf,ax=ax1, legend=True)
+   
+    ax1[0].set_title('Original Distributions')
+    ax1[1].set_title('Heatmap Correlation')
+    
+    ds_map = sns.kdeplot(data = tempdf,ax=ax1[0], legend=True, multiple="stack",fill=True, Alpha=.3, linewidth=0)
+    ns_heatmap = sns.heatmap(tempdf.corr(),ax=ax1[1], annot=True, center=0);
+    pp_map = sns.pairplot(dataframe, diag_kind='kde',hue='Survived')
 
-    sns.pairplot(dataframe , diag_kind='kde'
+   
+    ds_map
+    ns_heatmap
+    pp_map
 
     
 def buildgraph(dataframe):
